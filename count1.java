b48e9ab67903a27b6790fed69cc6bb29adee821d
Akhilaponnam <akhilaponnam@gmail.com>
2019-12-27 12:51:25 +0530

    ddd

---
+++ count1.java
@@ -0,0 +1,15 @@
+public class count1 {
+		public static void main(String[] args) {
+					int i = 100;
+							int count = 0;
+									while (i <= 500) {
+													if (i % 5 == 0) {
+																		count++;
+																					}
+																i++;
+																			}
+											System.out.println(count);
+												}
+
+}
+
